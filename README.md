16d20167d4b6b14136b14a1bb53fdd5dbc6652e1
j-e-d <j-e-d@users.noreply.github.com>
2024-01-10 17:33:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Guarda la información de la página de la agenda AFA
+
+Fuente: https://www.afa.com.ar/es/posts/la-agenda-de-la-afa
---
+++ get_text_file.py
@@ -0,0 +1,47 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup, Comment, NavigableString
+
+
+def tag_visible(element):
+    if isinstance(element, NavigableString):
+        if element.parent.name in [
+            "style",
+            "script",
+            "head",
+            "title",
+            "meta",
+            "[document]",
+        ]:
+            return False
+        if isinstance(element, Comment):
+            return False
+        return True
+    return False
+
+
+def get_visible_text(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    date_span = soup.select_one(".btArticleDate")
+    date_text = date_span.text if date_span is not None else ""
+    
+    div = soup.select_one(".btArticleContentInnerInner")
+    div_text = ""
+    if div is not None:
+        texts = div.findAll(string=True)
+        visible_texts = filter(tag_visible, texts)
+        div_text = "\n".join(re.sub(r'\s+', ' ', t) for t in visible_texts if t.strip())
+
+    return date_text + "\n" + div_text
+
+
+def get_clean_html(url, file_path):
+    clean_text = get_visible_text(url)
+    with open(file_path, "w") as f:
+        f.write(clean_text)
+
+# Use the function
+get_clean_html("https://www.afa.com.ar/es/posts/la-agenda-de-la-afa", "agenda.txt")
